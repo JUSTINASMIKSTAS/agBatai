789cdb0fe16b351182ea669493f6534e0e103f41
justinasmikstas <justinas.mikstas@gmail.com>
2024-02-03 13:19:56 +0200

    Add TestListener class

---
+++ src/test/java/lt/justinas/util/TestListener.java
@@ -0,0 +1,36 @@
+package lt.justinas.util;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+        takeScreenShot();
+    }
+
+    private void takeScreenShot() {
+        TakesScreenshot screenshot = (TakesScreenshot) Driver.getDriver();
+        File fileScreenshot = screenshot.getScreenshotAs(OutputType.FILE);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter dateForm = DateTimeFormatter.ofPattern("yyyyMMdd_HH_mm_ss_SSS");
+
+        File newFile = new File("./screenshots/" + currentDate.format(dateForm) + "screenshot.png");
+
+        try {
+            FileUtils.copyFile(fileScreenshot, newFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
