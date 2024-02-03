3491039fd9ca6876967eb6f66f2641168f80a48d
justinasmikstas <justinas.mikstas@gmail.com>
2024-02-03 19:23:03 +0200

    Add LoginPage and LoginTest class

---
+++ src/test/java/lt/justinas/page/agBatai/LoginPage.java
@@ -0,0 +1,6 @@
+package lt.justinas.page.agBatai;
+
+import lt.justinas.page.PageBase;
+
+public class LoginPage extends PageBase {
+}
---
+++ src/test/java/lt/justinas/test/agBatai/LoginTest.java
@@ -0,0 +1,12 @@
+package lt.justinas.test.agBatai;
+
+import lt.justinas.test.TestBase;
+import org.testng.annotations.BeforeMethod;
+
+public class LoginTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
+}
