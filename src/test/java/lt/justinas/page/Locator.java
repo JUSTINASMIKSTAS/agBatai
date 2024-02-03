d1b3b499e11898b16e03a25c8c483db69f8e1a79
justinasmikstas <justinas.mikstas@gmail.com>
2024-02-03 13:56:52 +0200

    Add Cookies xpath

--- src/test/java/lt/justinas/page/Locator.java
+++ src/test/java/lt/justinas/page/Locator.java
@@ -1,4 +1,12 @@
 package lt.justinas.page;
 
+import org.openqa.selenium.By;
+
 public class Locator {
+    public static class AgBatai{
+        public static class Registration{
+
+            public static By buttonCookie = By.xpath("//button[@id='lgcookieslaw_accept']");
+        }
+    }
 }
