33be95c130ac208ea5b264996bb1c94cca6b492e
justinasmikstas <justinas.mikstas@gmail.com>
2024-02-04 17:26:06 +0200

    ADD AddAddressTest and AddAddressPage class

---
+++ src/test/java/lt/justinas/test/agBatai/AddAddressTest.java
@@ -0,0 +1,12 @@
+package lt.justinas.test.agBatai;
+
+import lt.justinas.test.TestBase;
+import org.testng.annotations.BeforeMethod;
+
+public class AddAddressTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
+}
